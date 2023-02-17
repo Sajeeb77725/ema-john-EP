5c0c95b29c0bf8a444aac9d5dff9e62e4c14973d
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2023-02-17 14:00:48 +0600

    sign up form added

--- src/components/SingUp/SignUp.js
+++ src/components/SingUp/SignUp.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
   return (
-    <div>
-      <h2>This is sign Up.</h2>
+    <div className="form-container">
+      <div>
+        <h2>Sign Up</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" name="confirm-password" id="" />
+          </div>
+          <input className="form-submit" type="submit" value="Sign Up" />
+        </form>
+        <p>
+          Already have an account?{" "}
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
